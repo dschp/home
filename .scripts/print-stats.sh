36a5bb8604b062a73b10365aebb2e435ac53e507
Wyzd <dschp@dzy.me>
2025-10-13 03:28:53 +0000

    Brush up print scripts

--- .scripts/print-stats.sh
+++ .scripts/print-stats.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-DF_STAT=/tmp/df-stat.txt
+STATFILE=/tmp/print-stats.txt
 
-df -h 2>/dev/null | awk '$3 != 0' > $DF_STAT
-head -n1 $DF_STAT
-sed '1d' $DF_STAT | sort -k6,6
-#df -h
+df -h 2>/dev/null | awk '$3 != 0' | sed '1s/.*/\U&/' > $STATFILE
+head -n1 $STATFILE
+sed '1d' $STATFILE | sort -k6,6
 echo
 
-free -m
+free -m | sed -e '1s/^/MEMORY /' -e '1s/.*/\U&/' -e '2s/^Mem:/phys/' -e '3s/^Swap:/swap/' | column -t -R 2,3,4,5,6,7 > $STATFILE
+cat $STATFILE
 echo
 
-uptime
+uptime | column -t -o ' '
--- .scripts/print-temps.sh
+++ .scripts/print-temps.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-sensors $SENSORS_CHIPS | sed -e 's/(.*)//g' -e '/^  *$/d'
+#sensors $SENSORS_CHIPS | sed -e 's/(.*)//g' -e '/^  *$/d' -e 's/ *$//g' | column -t -L -s ':'
+
+for chip in $SENSORS_CHIPS; do
+	echo "[$chip]"
+    sensors $chip | sed -e '1d' -e '/Adapter:/d' -e 's/(.*)//g' -e '/^  *$/d' -e 's/ *$//g' -e 's/  */ /g' -e 's/ *$//g' | column -t -R 1,2 -s ':' -o ':' -d -C width=10 -C width=10
+	echo
+done
+
