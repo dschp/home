13205fa67d784a3d11543657dfe085578cf7a547
Wyzd <dschp@dzy.me>
2025-10-14 19:02:15 +0000

    Brush up print-temps.sh

--- .scripts/print-temps.sh
+++ .scripts/print-temps.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-#sensors $SENSORS_CHIPS | sed -e 's/(.*)//g' -e '/^  *$/d' -e 's/ *$//g' | column -t -L -s ':'
-
-for chip in $SENSORS_CHIPS; do
-	echo "[$chip]"
-    sensors $chip | sed -e '1d' -e '/Adapter:/d' -e 's/(.*)//g' -e '/^  *$/d' -e 's/ *$//g' -e 's/  */ /g' -e 's/ *$//g' | column -t -R 1,2 -s ':' -o ':' -d -C width=10 -C width=10
-	echo
-done
-
+sensors $SENSORS_CHIPS \
+| sed -e '/Adapter:/d' -e 's/(.*)//g' -e '/^  *$/d' -e 's/ *$//g' -e 's/  */ /g' -e 's/ *$//g' \
+| awk -F : '{
+	if ($0 ~ /^ *$/) {
+		print ""
+	} else if ($0 ~ /.+:./) {
+		printf("%10s:%10s\n", $1, $2)
+	} else {
+		print "[" $0 "]"
+	}
+}'
