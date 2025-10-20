7bc6383dce0fe41ba3f2e6426f5764ef27765ec9
Wyzd <dschp@dzy.me>
2025-10-20 02:58:09 +0000

    Tweak print-procs

--- .scripts/print-procs.sh
+++ .scripts/print-procs.sh
@@ -2,6 +2,6 @@
 
 STATFILE=/tmp/print-ps.txt
 
-ps aux | awk '$3 != 0.0' | column -t -l 11 -R 2,3,4,5,6,9,10 > $STATFILE
+ps aux | awk '$3 != 0.0 && $4 != 0.0' | column -t -l 11 -R 2,3,4,5,6,9,10 > $STATFILE
 head -n1 $STATFILE
 sed '1d' $STATFILE | sort -k3,3nr
