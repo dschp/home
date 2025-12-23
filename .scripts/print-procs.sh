2d3e9b6f3ee2d4cb8e8672f881836a47f9d5fb52
dzywin <dschp@dzy.me>
2025-12-24 04:21:18 +0700

    Update print-procs

--- .scripts/print-procs.sh
+++ .scripts/print-procs.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-STATFILE=/tmp/print-ps.txt
-
-ps aux | awk '$3 != 0.0 && $4 != 0.0' | column -t -l 11 -R 2,3,4,5,6,9,10 > $STATFILE
-head -n1 $STATFILE
-sed '1d' $STATFILE | sort -k3,3nr
+STAT="$(ps aux | awk '$3 != 0.0 && $4 != 0.0' | column -t -l 11 -R 2,3,4,5,6,9,10)"
+echo "$STAT" | head -n1
+echo "$STAT" | sed '1d' | sort -k3,3nr
