ded25e256b994582ffad44f9397439e431709694
dzywin <dschp@dzy.me>
2025-12-23 11:42:43 +0700

    Update print-stats

--- .scripts/print-stats.sh
+++ .scripts/print-stats.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 
-STATFILE=/tmp/print-stats.txt
-
-df -h 2>/dev/null | awk '$3 != 0' | sed '1s/.*/\U&/' > $STATFILE
-head -n1 $STATFILE
-sed '1d' $STATFILE | sort -k6,6
+DF="$(df -h 2>/dev/null | awk '$3 != 0' | sed '1s/.*/\U&/')"
+echo "$DF" | head -n1
+echo "$DF" | sed '1d' | sort -k6,6
 echo
 
-free -m | sed -e '1s/^/MEMORY /' -e '1s/.*/\U&/' -e '2s/^Mem:/phys/' -e '3s/^Swap:/swap/' | column -t -R 2,3,4,5,6,7 > $STATFILE
-cat $STATFILE
+free -m | sed -e '1s/^/MEMORY /' -e '1s/.*/\U&/' -e '2s/^Mem:/phys/' -e '3s/^Swap:/swap/' | column -t -R 2,3,4,5,6,7
 echo
 
 uptime | column -t -o ' '
