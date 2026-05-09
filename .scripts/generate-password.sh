60af963e8675099a3e9776803ca81cb27d133212
monte <dschp@dzy.me>
2026-05-09 20:23:11 +0900

    Update generate-password.sh

--- .scripts/generate-password.sh
+++ .scripts/generate-password.sh
@@ -1,5 +1,13 @@
-#!/bin/sh
-for i in `seq 1 10`
-do
-	tr -dc 'A-Za-z0-9@_!#%.,;+-' </dev/urandom | head -c 30; echo
+#!/bin/bash
+
+chars=(
+	"A-Za-z0-9@_!#%.,;+-"
+	"A-Za-z0-9"
+)
+
+for c in "${chars[@]}"; do
+	for i in `seq 1 6`; do
+		tr -dc "${c}" </dev/urandom | head -c 30; echo
+	done
+	echo
 done
